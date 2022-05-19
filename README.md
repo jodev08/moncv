09f5d89064462ec3063f9c36ffff27cad040c9d5
jodev08 <j.stonskadev@gmail.com>
2022-05-19 16:19:48 +0200

    suite css page accueil

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ style->
 
 style global : style.scss
 reglage global : \_settings.scss
+
+stop 1:40:00
--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -3,8 +3,20 @@ import Navigation from "../components/Navigation";
 
 const Accueil = () => {
   return (
-    <div>
+    <div className="home">
       <Navigation></Navigation>
+
+      <div className="homeContent">
+        <div className="content">
+          <h1>Stonska Jordan</h1>
+          <h2>Développeur</h2>
+          <div className="pdf">
+            <a href="./media/CV_StonskaJordan.pdf" target="_blank">
+              Télécharger cv
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
